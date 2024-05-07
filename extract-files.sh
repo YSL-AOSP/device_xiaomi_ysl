xiaomi-msm8953-devs/android_device_xiaomi_ysl

ysl: Build biometrics hal
Change-Id: I165682740ade433e1c8586ba6267f84ea12de8c0

extract-files.sh
@@ -11,6 +11,9 @@ function blob_fixup() {
         vendor/lib64/hw/consumerir.msm8953.so)
             sed -i "s|/dev/spidev6.1|/dev/spidev5.1|g" "${2}"
             ;;
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            ;;
     esac
 }
 

YSL-AOSP/device_xiaomi_ysl

extract-files.sh
@@ -11,6 +11,9 @@ function blob_fixup() {
         vendor/lib64/hw/consumerir.msm8953.so)
             sed -i "s|/dev/spidev6.1|/dev/spidev5.1|g" "${2}"
             ;;
+        vendor/lib/libvendor.goodix.hardware.fingerprint@1.0-service.so)
+            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            ;;
     esac
 }
 